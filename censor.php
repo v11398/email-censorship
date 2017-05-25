bfa52d6809324bb802685992225f67fb69ab4600
Alfredo Vidueira <vidueira_1992@hotmail.com>
2017-05-24 23:55:54 -0400

    Update censor.php

--- censor.php
+++ censor.php
@@ -1,8 +1,12 @@
 <?php
-$email = 'example@example.com';
-$at = strpos($email, '@');
-$partOne = substr($email, 0, round($at / 2.0, 0, PHP_ROUND_HALF_DOWN));
-$censored = str_repeat('*', round($at / 2.0, 0, PHP_ROUND_HALF_UP));
-$partTwo = substr($email, $at);
-$email = $partOne.$censored.$partTwo;
-echo $email;
+function censorEmail($email)
+{
+	$at = strpos($email, '@');
+	$partOne = substr($email, 0, round($at / 2, 0, PHP_ROUND_HALF_DOWN));
+	$censored = str_repeat('*', round($at / 2, 0, PHP_ROUND_HALF_UP));
+	$partTwo = substr($email, $at);
+	$email = $partOne.$censored.$partTwo;
+	return $email;
+}
+
+echo censorEmail('example@example.com');
